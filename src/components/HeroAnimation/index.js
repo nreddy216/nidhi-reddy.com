26e791e2ba600f73250072dd52b443903d734127
nreddy216 <nreddy216@gmail.com>
2020-11-12 18:42:28 -0600

    Add anim interaction

--- src/components/HeroAnimation/index.js
+++ src/components/HeroAnimation/index.js
@@ -85,13 +85,13 @@ class ThreeAnimation extends React.Component {
     let floorGeometry = new THREE.PlaneGeometry(400, 150, 1, 1);
     let floorMaterial = new THREE.MeshPhongMaterial({
       color: 0xfa8072,
-      shininess: 0,
+      shininess: 0
     });
 
     let floor = new THREE.Mesh(floorGeometry, floorMaterial);
     floor.rotation.x = -0.5 * Math.PI; // This is 90 degrees by the way
     floor.receiveShadow = true;
-    floor.position.y = -5;
+    floor.position.y = -30;
     scene.add(floor);
 
     function update() {
@@ -147,29 +147,38 @@ class ThreeAnimation extends React.Component {
     const stacy_mtl = new THREE.MeshPhongMaterial({
       map: stacy_txt,
       color: 0xffffff,
-      skinning: true,
+      skinning: true
     });
 
     var loader = new THREEGLTFLoader();
     var self = this;
     loader.load(
       MODEL_PATH,
-      function (gltf) {
+      function(gltf) {
         // A lot is going to happen here
         model = gltf.scene;
         let fileAnimations = gltf.animations;
 
-        model.traverse((o) => {
+        model.traverse(o => {
           if (o.isMesh) {
             o.castShadow = true;
             o.receiveShadow = true;
             o.material = stacy_mtl;
           }
+
+          // Reference the neck and waist bones
+          if (o.isBone && o.name === "mixamorigNeck") {
+            neck = o;
+          }
+          if (o.isBone && o.name === "mixamorigSpine") {
+            waist = o;
+          }
         });
 
         // Set the models initial scale
-        model.scale.set(10, 10, 10);
-        model.position.y = -6;
+        model.scale.set(20, 20, 20);
+        model.position.y = -30;
+        model.position.z = 30;
 
         // Add model to scene
         scene.add(model);
@@ -179,14 +188,78 @@ class ThreeAnimation extends React.Component {
 
         mixer = new THREE.AnimationMixer(model);
         let idleAnim = THREE.AnimationClip.findByName(fileAnimations, "idle");
+        idleAnim.tracks.splice(3, 3);
+        idleAnim.tracks.splice(9, 3);
         idle = mixer.clipAction(idleAnim);
         idle.play();
       },
       undefined, // We don't need this function
-      function (error) {
+      function(error) {
         console.error(error);
       }
     );
+
+    document.addEventListener("mousemove", function(e) {
+      var mousecoords = getMousePos(e);
+      if (neck && waist) {
+        moveJoint(mousecoords, neck, 50);
+        moveJoint(mousecoords, waist, 30);
+      }
+    });
+
+    function getMousePos(e) {
+      return { x: e.clientX, y: e.clientY };
+    }
+
+    function moveJoint(mouse, joint, degreeLimit) {
+      let degrees = getMouseDegrees(mouse.x, mouse.y, degreeLimit);
+      joint.rotation.y = THREE.Math.degToRad(degrees.x);
+      joint.rotation.x = THREE.Math.degToRad(degrees.y);
+    }
+
+    function getMouseDegrees(x, y, degreeLimit) {
+      let dx = 0,
+        dy = 0,
+        xdiff,
+        xPercentage,
+        ydiff,
+        yPercentage;
+
+      let w = { x: window.innerWidth, y: window.innerHeight };
+
+      // Left (Rotates neck left between 0 and -degreeLimit)
+
+      // 1. If cursor is in the left half of screen
+      if (x <= w.x / 2) {
+        // 2. Get the difference between middle of screen and cursor position
+        xdiff = w.x / 2 - x;
+        // 3. Find the percentage of that difference (percentage toward edge of screen)
+        xPercentage = (xdiff / (w.x / 2)) * 100;
+        // 4. Convert that to a percentage of the maximum rotation we allow for the neck
+        dx = ((degreeLimit * xPercentage) / 100) * -1;
+      }
+      // Right (Rotates neck right between 0 and degreeLimit)
+      if (x >= w.x / 2) {
+        xdiff = x - w.x / 2;
+        xPercentage = (xdiff / (w.x / 2)) * 100;
+        dx = (degreeLimit * xPercentage) / 100;
+      }
+      // Up (Rotates neck up between 0 and -degreeLimit)
+      if (y <= w.y / 2) {
+        ydiff = w.y / 2 - y;
+        yPercentage = (ydiff / (w.y / 2)) * 100;
+        // Note that I cut degreeLimit in half when she looks up
+        dy = ((degreeLimit * 0.5 * yPercentage) / 100) * -1;
+      }
+
+      // Down (Rotates neck down between 0 and degreeLimit)
+      if (y >= w.y / 2) {
+        ydiff = y - w.y / 2;
+        yPercentage = (ydiff / (w.y / 2)) * 100;
+        dy = (degreeLimit * yPercentage) / 100;
+      }
+      return { x: dx, y: dy };
+    }
   }
 
   shouldComponentUpdate(nextProps, nextState) {
@@ -195,9 +268,9 @@ class ThreeAnimation extends React.Component {
 
   render() {
     return (
-      <div ref={(ref) => (this.mount = ref)}>
+      <div ref={ref => (this.mount = ref)}>
         {/* TODO: Add loading animation */}
-        <Styled.LoaderAnim ref={(ref) => (this.loaderAnim = ref)}>
+        <Styled.LoaderAnim ref={ref => (this.loaderAnim = ref)}>
           <Styled.Loader>Loading...</Styled.Loader>
         </Styled.LoaderAnim>
       </div>
@@ -235,9 +308,9 @@ const HeroAnimation = () => {
               maxValue={24}
               handle1={{
                 value: value1,
-                onChange: (v) => {
+                onChange: v => {
                   setValue1(v);
-                },
+                }
               }}
               onControlFinished={() => setEndingValue(value1)}
               arcColor="#48bb78"
